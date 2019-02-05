90a671b19c20157469a96dca126fbe41b771d4c0
Rajesh Singh <r17_singh@yahoo.com>
2019-02-04 19:51:13 -0800

    .

---
+++ src/components/autosuggest.test.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom'
+import Autosugget from "./autosuggest";
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import renderer from 'react';
+import { mount } from 'enzyme';
+
+configure({ adapter: new Adapter() });
+
+describe("Auto suggest component", () => {
+
+    //Shallow Rendering
+    test("renders", () => {
+        const wrapper = shallow(<Autosugget/>);
+        expect(wrapper.exists()).toBe(true);
+    });
+
+    //Full DOM Rendering
+    test('allows set props', () => {
+        const wrapper = mount(<Autosugget type="normal" data= {[]} />);
+        expect(wrapper.props().type).toEqual('normal');
+        wrapper.setProps({ type: 'normal' });
+        const dataIn = ["Africa","America","Asia","Europe","Oceania"];
+        wrapper.setProps({ data: dataIn });
+        expect(wrapper.props().data).toEqual(dataIn);
+      });
+    
+});
